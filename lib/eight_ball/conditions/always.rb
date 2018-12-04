19ab1ace444ae0cd4aa6f14e7dfbce82b5f7652a
Marc Morinville <marc@rewind.io>
2018-12-04 17:06:44 -0500

    Fix typo in comment

--- lib/eight_ball/conditions/always.rb
+++ lib/eight_ball/conditions/always.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module EightBall::Conditions
-  # The Always Conditions is always satisfied
+  # The Always Condition is always satisfied
   class Always < Base
     def satisfied?
       true
