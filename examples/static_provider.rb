260dc39d45546b2661a04367ed5bcfa0212aee03
Marc Morinville <marc@rewind.io>
2020-06-16 14:05:07 -0400

    Fix example

--- examples/static_provider.rb
+++ examples/static_provider.rb
@@ -2,7 +2,7 @@ require 'eight_ball'
 
 # Create a Feature programatically
 enabled_for = EightBall::Conditions::Range.new min: 1, max: 10, parameter: 'account_id'
-disabled_for = EightBall::Conditions::List.new values: [2, 3]
+disabled_for = EightBall::Conditions::List.new values: [2, 3], parameter: 'account_id'
 feature = EightBall::Feature.new 'Feature1', enabled_for, disabled_for
 
 # Tell EightBall about the Features
